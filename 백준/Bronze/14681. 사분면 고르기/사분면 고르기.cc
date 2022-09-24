db39697db7f7188ef22519f06ebd92fbdce2cda9
delver <98009770+delvering17@users.noreply.github.com>
2022-09-25 02:08:37 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int x, y;
+   int quadrant;
+   cin >> x >> y;   
+   if(x > 0 && y > 0) {
+      quadrant = 1;
+   } else if (x < 0 && y > 0) {
+      quadrant = 2;
+   } else if (x < 0 && y < 0) {
+      quadrant = 3;
+   } else {
+      quadrant = 4;
+   }
+
+   cout << quadrant;
+   return 0;
+}
