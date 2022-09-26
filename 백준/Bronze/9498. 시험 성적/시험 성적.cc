8322d6c55892dc41ca38c502e2289e549f5c30f2
delver <98009770+delvering17@users.noreply.github.com>
2022-09-27 00:45:03 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int point;
+    cin >> point;
+
+    if(point >= 90) cout << 'A'; 
+    else if (point >= 80) cout << "B";
+    else if (point >= 70) cout << "C";
+    else if (point >= 60) cout << "D";
+    else cout << "F";
+
+    return 0;
+}
