f31f1c5165d0a5b157a8efc29d492aaec182daf0
delver <98009770+delvering17@users.noreply.github.com>
2022-09-26 18:10:36 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N;
+  int X;
+  cin >> N >> X;
+  
+  for(int i = 0 ; i < N ; i++) {
+    int num;
+    cin >> num;
+    if(num < X) {
+      cout << num << " ";
+    }
+  }
+  
+  return 0;
+}
