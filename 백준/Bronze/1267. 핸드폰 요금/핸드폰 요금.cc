53892bd55707ed844db5f6e86be7617e31ad81e7
delver <98009770+delvering17@users.noreply.github.com>
2022-09-27 16:19:18 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cal(int arr[], int cnt, int time, int price) {
+    int pay = 0;
+
+    for(int i = 0 ; i < cnt ; i++) {
+        pay += (arr[i] / time) * price + price ;
+    }
+    return pay;
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int cnt;
+    cin >> cnt;
+
+    int arr[cnt];
+    for(int i = 0 ; i < cnt ; i++) {
+        cin >> arr[i];
+    }
+    
+    int Y = cal(arr, cnt, 30, 10);
+    int M = cal(arr, cnt, 60, 15);
+    
+    if(Y < M) cout << "Y " << Y;
+    else if(Y > M) cout << "M " << M;
+    else cout << "Y M " << Y;
+
+    return 0;
+}
