8373d3136e0ea87eb6eb0e0bb0858dafd4be8378
delver <98009770+delvering17@users.noreply.github.com>
2022-09-27 15:24:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -6,28 +6,20 @@ int main(void) {
     cin.tie(0);
     
     int arr[9];
-    int real[7];
+    int sum = 0;
 
-    for(int i = 0 ; i < 9 ; i++) cin >> arr[i];
+    for(int i = 0 ; i < 9 ; i++) cin >> arr[i], sum += arr[i];
 
     for(int i = 0 ; i < 9 ; i++) {
-        int sum;
         for(int j = i + 1 ; j < 9 ; j++) {
-            sum = 0;
-
-            for(int k = 0, idx = 0 ; k < 9 ; k++) {
-                if(k != i && k != j) {
-                    real[idx++] = arr[k];
-                    sum += arr[k];
-                }
+            if(sum - arr[i] - arr[j] == 100) {
+                arr[i] = arr[j] = 0;
+                i = j = 9;
             }
-            if(sum == 100) break;
         }
-        if(sum == 100) break;
     }
-
-    sort(real, real + 7);
-    for(int num : real) cout << num << "\n";
+    sort(arr, arr + 9);
+    for(int i = 2 ; i < 9 ; i++) cout << arr[i] << "\n";
     
     return 0;
 }
