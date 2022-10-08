c8d7f2a254d426f482ba110c07968dce64fa456d
delver <98009770+delvering17@users.noreply.github.com>
2022-10-08 18:33:03 +0900

    [Silver II] Title: 에디터, Time: 40 ms, Memory: 7576 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 1000005;
+const int MX = 600001;
 char dat[MX];
-int pre[MX]; 
+int pre[MX];
 int nxt[MX];
 int unused = 1;
+int cursor;
 
-void insert(int addr, int num){
-  dat[unused] = num;
+void insert(int addr, char c) {
+  dat[unused] = c;
   pre[unused] = addr;
   nxt[unused] = nxt[addr];
-  if(nxt[addr] != -1) pre[nxt[addr]] = unused;
+  if(nxt[addr] != -1) pre[nxt[addr]] = unused; 
   nxt[addr] = unused;
+  cursor = unused;
   unused++;
 }
 
-void erase(int addr){
+void remove(int addr) {
   nxt[pre[addr]] = nxt[addr];
   if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+  cursor = pre[addr];
 }
 
-void traversal(){
+void traverse() {
   int cur = nxt[0];
-  while(cur != -1){
+  while(cur != -1) {
     cout << dat[cur];
     cur = nxt[cur];
   }
@@ -32,38 +35,40 @@ void traversal(){
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  fill(pre,pre+MX,-1);
-  fill(nxt,nxt+MX,-1);
-  string init;
-  cin >> init;
-  int cursor = 0;
-  for(auto c : init){
-    insert(cursor, c);
-    cursor++;
-  }  
-  int q;
-  cin >> q;
-  while (q--) {
-    char op;
-    cin >> op;
-    if (op == 'P') {
-      char add;
-      cin >> add;
-      insert(cursor, add);
-      cursor = nxt[cursor];
+  fill(pre, pre+MX, -1);
+  fill(nxt, nxt+MX, -1);
+
+  string str;
+  cin >> str;
+  for(auto c : str) insert(unused-1, c);
+  cursor = unused-1;
+
+  int time;
+  cin >> time;
+  
+  for(int i = 0 ; i < time ; i++) {
+    char c;
+    cin >> c;
+    switch(c){
+      case 'L':
+        if(pre[cursor] != -1) cursor = pre[cursor];
+        break;
+      case 'D':
+        if(nxt[cursor] != -1) cursor = nxt[cursor];
+        break;
+      case 'B':
+        if(pre[cursor] != -1) remove(cursor);
+        break;  
+      case 'P':
+        char h;
+        cin >> h;
+        insert(cursor, h);
+        break;
     }
-    else if (op == 'L') {
-      if (pre[cursor] != -1) cursor = pre[cursor];
-    }
-    else if (op == 'D') {
-      if (nxt[cursor] != -1) cursor = nxt[cursor];
-    }
-    else { // 'B'
-      if (pre[cursor] != -1) {
-        erase(cursor);
-        cursor = pre[cursor];
-      }
-    }
+
   }
-  traversal();
+
+  traverse();
+
+  return 0;
 }
