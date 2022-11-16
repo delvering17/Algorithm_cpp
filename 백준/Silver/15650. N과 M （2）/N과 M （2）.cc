db1f343ed474514192c32d7348de43e149a8bb66
delver <98009770+delvering17@users.noreply.github.com>
2022-11-16 18:06:18 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isused[10];
+
+void func(int k, int num) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = num; i <= n; i++) {
+        if (!isused[i]) {
+            arr[k] = i;
+            isused[i] = true;
+            func(k+1, i + 1);
+            isused[i] = false;
+        }
+    }
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    func(0, 1);
+    return 0;
+}
